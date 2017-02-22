982a0e76e032e6fa70ebef622a89d35ee6f257f0
Luke Winslow <lawinslow@gmail.com>
2017-02-22 09:45:05 -0500

    Add additional variables and project cleanup
    - Add and fix some of the query functions
    - Add updated PDSI, TMP, PCP
    - Get rid of boilerplate hello function

--- R/AAA.R
+++ R/AAA.R
@@ -4,7 +4,7 @@ nclimdiv = new.env()
 
 nclimdiv$shape = rgdal::readOGR(system.file('extdata', 'climdivisions', package='nclimdivr'), layer = 'GIS.OFFICIAL_CLIM_DIVISIONS')
 
-d = read.table(system.file('extdata', 'climdiv-tmpcdv-v1.0.0-20160404', package='nclimdivr'), header=FALSE, colClasses=c('character', rep('numeric',12)))
+d = read.table(system.file('extdata', 'climdiv-tmpcdv-v1.0.0-20170206', package='nclimdivr'), header=FALSE, colClasses=c('character', rep('numeric',12)))
 
 names(d) = c('metadata', paste0('X', 1:12))
 d = as.data.frame(lapply(d, function(x){x[x < -99] = NA; x}))
@@ -18,4 +18,38 @@ d_long$year = as.numeric(substr(d_long$metadata,7,10))
 d_long$CLIMDIV = as.numeric(substr(d_long$metadata,1,4))
 
 nclimdiv$tmp = d_long
+
+
+## snow
+d = read.table(system.file('extdata', 'climdiv-pcpndv-v1.0.0-20170206', package='nclimdivr'), header=FALSE, colClasses=c('character', rep('numeric',12)))
+
+names(d) = c('metadata', paste0('X', 1:12))
+d = as.data.frame(lapply(d, function(x){x[x < -99] = NA; x}))
+
+d_long = reshape2::melt(d, id.vars='metadata', value.name = 'pcp')
+d_long$month = as.numeric(gsub('X', '', d_long$variable))
+d_long$variable = NULL
+d_long$pcp = (d_long$pcp/100)*0.0254
+
+d_long$year = as.numeric(substr(d_long$metadata,7,10))
+d_long$CLIMDIV = as.numeric(substr(d_long$metadata,1,4))
+
+nclimdiv$pcp = d_long
+
+
+## PDSI
+d = read.table(system.file('extdata', 'climdiv-pdsidv-v1.0.0-20170206', package='nclimdivr'), header=FALSE, colClasses=c('character', rep('numeric',12)))
+
+names(d) = c('metadata', paste0('X', 1:12))
+d = as.data.frame(lapply(d, function(x){x[x < -99] = NA; x}))
+
+d_long = reshape2::melt(d, id.vars='metadata', value.name = 'pdsi')
+d_long$month = as.numeric(gsub('X', '', d_long$variable))
+d_long$variable = NULL
+#d_long$pdsi = (d_long$pdsi/100)*0.0254
+
+d_long$year = as.numeric(substr(d_long$metadata,7,10))
+d_long$CLIMDIV = as.numeric(substr(d_long$metadata,1,4))
+
+nclimdiv$pdsi = d_long
 
